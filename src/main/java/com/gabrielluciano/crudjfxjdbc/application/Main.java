f81cb364daef8e02b905091a74e7a1d8ad68e3fe
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-07-06 11:38:22 -0300

    main view design

--- src/main/java/com/gabrielluciano/crudjfxjdbc/application/Main.java
+++ src/main/java/com/gabrielluciano/crudjfxjdbc/application/Main.java
@@ -2,8 +2,8 @@ package com.gabrielluciano.crudjfxjdbc.application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +12,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
-            Parent parent = loader.load();
+            ScrollPane scrollPane = loader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(parent);
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Crud App with JavaFX and JDBC");
             primaryStage.show();
