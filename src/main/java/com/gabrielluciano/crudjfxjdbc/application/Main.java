64c9b478d3f376c2ec63b15607653ae0b005999c
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-07-06 11:29:29 -0300

    creates MainView

--- src/main/java/com/gabrielluciano/crudjfxjdbc/application/Main.java
+++ src/main/java/com/gabrielluciano/crudjfxjdbc/application/Main.java
@@ -1,19 +1,23 @@
 package com.gabrielluciano.crudjfxjdbc.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         try {
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root, 400, 400);
-            stage.setScene(scene);
-            stage.show();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+            Parent parent = loader.load();
+
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Crud App with JavaFX and JDBC");
+            primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
