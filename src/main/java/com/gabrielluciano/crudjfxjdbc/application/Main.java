131a9eacc2c54966adf967ad348b6de56c8753ef
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-07-07 15:35:50 -0300

    About view

--- src/main/java/com/gabrielluciano/crudjfxjdbc/application/Main.java
+++ src/main/java/com/gabrielluciano/crudjfxjdbc/application/Main.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
@@ -16,13 +18,17 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Crud App with JavaFX and JDBC");
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
