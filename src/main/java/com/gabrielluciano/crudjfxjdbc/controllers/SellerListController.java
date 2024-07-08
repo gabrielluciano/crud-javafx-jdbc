5f0a0938baa967aacebd3ef1c7f64f79c0ef81f7
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-07-08 12:38:37 -0300

    seller TableView

--- src/main/java/com/gabrielluciano/crudjfxjdbc/controllers/SellerListController.java
+++ src/main/java/com/gabrielluciano/crudjfxjdbc/controllers/SellerListController.java
@@ -1,7 +1,7 @@
 package com.gabrielluciano.crudjfxjdbc.controllers;
 
-import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -19,9 +19,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -29,8 +27,6 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SellerListController implements Initializable, DataChangeListener {
@@ -45,6 +41,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 
     @FXML
     private TableColumn<Seller, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Seller, LocalDate> tableColumnBirthDate;
+
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
 
     @FXML
     private TableColumn<Seller, Seller> tableColumnEdit;
@@ -75,6 +80,11 @@ public class SellerListController implements Initializable, DataChangeListener {
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        GUIUtils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        GUIUtils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
--- src/main/java/com/gabrielluciano/crudjfxjdbc/util/GUIUtils.java
+++ src/main/java/com/gabrielluciano/crudjfxjdbc/util/GUIUtils.java
@@ -1,12 +1,55 @@
 package com.gabrielluciano.crudjfxjdbc.util;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class GUIUtils {
 
     public static Stage currentStage(ActionEvent event) {
         return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+
+    public static <T> void formatTableColumnDate(TableColumn<T, LocalDate> tableColumn, String pattern) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, LocalDate> cell = new TableCell<T, LocalDate>() {
+                private DateTimeFormatter fmt = DateTimeFormatter.ofPattern(pattern);
+
+                @Override
+                protected void updateItem(LocalDate item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(fmt.format(item));
+                    }
+                }
+            };
+            return cell;
+        });
+    }
+
+    public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Double> cell = new TableCell<T, Double>() {
+                @Override
+                protected void updateItem(Double item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        Locale.setDefault(Locale.US);
+                        setText(String.format("%." + decimalPlaces + "f", item));
+                    }
+                }
+            };
+            return cell;
+        });
     }
 }
